6412721559e6553fcd0752884969d74f7b92a486
MAHABRI ARMAN <40721240084@svkmgrp.com>
2026-04-20 15:37:15 +0530

    added button click handler to capture city input

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded",() =>{
     const cityInput = document.getElementById("city-input")
     const getWeatherBtn = document.getElementById("get-weather-btn")
 
+    getWeatherBtn.addEventListener("click", ()=>{
+        const city = cityInput.value.trim()
+        console.log(city)
+    })
 })
