27c0772fa14742956ffca0f6c8f47f5c6ffc4bdc
MAHABRI ARMAN <40721240084@svkmgrp.com>
2026-04-20 15:19:02 +0530

    setup DOM refrenece for weather App

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    const cityInput = document.getElementById("city-input")
+    const getWeatherBtn = document.getElementById("get-weather-btn")
+
+})
